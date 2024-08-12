ee64124c1c9756fa564a2c48109a5c869637caa2
Brian Clark <131870089+BrianClark04@users.noreply.github.com>
2024-08-12 15:58:57 -0400

    A

---
+++ BACKEND/Conexion/Conexion.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+
+ini_set('log_errors', '1');
+ini_set('error_log', '../log/php_errors.log0');
+
+class conn
+{
+
+
+    function connection()
+    {
+
+        $host = "localhost";
+        $usuario = "root";
+        $password = "";
+        $bd = "biblioteca";
+        $puerto = 3306;
+        try {
+            $mysql = new mysqli($host, $usuario, $password, $bd, $puerto);
+            return $mysql;
+
+        } catch (Exception $e) {
+            echo 'Excepción capturada: ', $e->getMessage(), "\n";
+        }
+
+
+    }
+
+
+}
+
+
+?>
